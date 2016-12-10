4f0664a54fd1a5385827c06cc9f989f708759e0d
Adam111 <Adam111@ADAM>
2016-12-10 18:47:43 +0100

    Mail service fasz started

---
+++ src/Heinbo/Services/MailService.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heinbo.Services
+{
+    public class MailService : IMailService
+    {
+        public async Task SendEmailAsync(string to,string from, string subject, string message, string sender)
+        {
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress(sender, from));
+            emailMessage.To.Add(new MailboxAddress("", to));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart("plain") { Text = message };
+
+            using (var client = new SmtpClient())
+            {
+                client.LocalDomain = "some.domain.com";
+                await client.ConnectAsync("smtp.relay.uri", 25, SecureSocketOptions.Auto).ConfigureAwait(false);
+                await client.SendAsync(emailMessage).ConfigureAwait(false);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ src/Heinbo/wwwroot/js/contactPageController.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+    angular.module("app-contactPage")
+        .controller("contactPageController", contactPageController);
+
+    function contactPageController($http, $scope, $window) {
+        var vm = this;
+        vm.errorMessage = "";
+        vm.isBusy = true;
+        
+    }
+})();
